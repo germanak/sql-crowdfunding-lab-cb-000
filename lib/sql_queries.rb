691502b1a7d0de7906f7aad5253c7f29b7207785
Alec German <alec_german@yahoo.com>
2018-03-13 23:27:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,8 +28,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount)
 FROM projects
 INNER JOIN pledges
-ON pledges.project_id = project_id
-WHERE SUM(projects.funding_goal) >= SUM(pledges.amount)"
+ON SUM(projects.funding_goal) >= SUM(pledges.amount)"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
