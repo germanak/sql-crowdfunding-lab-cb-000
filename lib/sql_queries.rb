0ba002747f40ca50fef1a72c5514f16f70ccf380
Alec German <alec_german@yahoo.com>
2018-03-13 23:41:42 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,15 +25,15 @@ ORDER BY users.name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount)
-FROM projects
-INNER JOIN pledges
-ON pledges.project_id = projects.id
-GROUP BY projects.title"
+""
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amounts)
+FROM users
+INNER JOIN pledges
+ON pledges.user_id = users.id
+GROUP BY users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
