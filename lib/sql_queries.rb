45f67ff6b527e2a857b36a0e35a52e94fa0bcae8
Alec German <alec_german@yahoo.com>
2018-03-13 23:55:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ ORDER BY SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
 FROM projects
 INNER JOIN pledges
 ON pledges.project_id = projects.id WHERE projects.category = 'music'"
