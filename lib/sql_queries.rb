6209c6430c23235254936a8dbe0cf4f14d955fb5
Alec German <alec_german@yahoo.com>
2018-03-13 23:34:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT projects.title, SUM(pledges.amount)
 FROM projects
-INNER JOIN pledges
+LEFT INNER JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY projects.title"
 end
